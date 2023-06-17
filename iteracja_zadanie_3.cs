26bc4241758ea84961dfa75cc5341cbaf2b176b9
dsmylaWSB <chx99935@student.chorzow.wsb.pl>
2023-06-17 16:13:12 +0200

    podczas zajęć

---
+++ iteracja_zadanie_3.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Liczba wierszy: ");
+int n = int.Parse(Console.ReadLine());
+
+
+for (int i = 0; i <= n; i++)
+{
+    for (int j = 1; j <= n - i; j++)
+        Console.Write(" ");
+    for ( int j = 1; j <= 2 * i - 1; j++)
+        Console.Write("+");
+    Console.Write("\n");
+}
